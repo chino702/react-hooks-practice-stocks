37df5418f558bacfa88b09616e442b9458da2bd0
chino702 <chinox3702@gmail.com>
2023-08-09 15:42:57 -0700

    finished the react stock practice

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [filteredStocks, setFilteredStocks] = useState([]);
+
+  useEffect(() => {
+    // Fetch stocks from API
+    fetch("http://localhost:3001/stocks")
+      .then((response) => response.json())
+      .then((data) => {
+        setStocks(data);
+        setFilteredStocks(data);
+      });
+  }, []);
+
+  const buyStock = (stock) => {
+    setPortfolio([...portfolio, stock]);
+  };
+
+  const sellStock = (stockId) => {
+    const updatedPortfolio = portfolio.filter((stock) => stock.id !== stockId);
+    setPortfolio(updatedPortfolio);
+  };
+
+  const handleSortByName = () => {
+    const sortedStocks = [...filteredStocks].sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setFilteredStocks(sortedStocks);
+  };
+
+  const handleSortByPrice = () => {
+    const sortedStocks = [...filteredStocks].sort((a, b) => a.price - b.price);
+    setFilteredStocks(sortedStocks);
+  };
+
+  const handleFilter = (event) => {
+    const selectedType = event.target.value;
+    if (selectedType === "All") {
+      setFilteredStocks(stocks);
+    } else {
+      const filteredStocks = stocks.filter((stock) => stock.type === selectedType);
+      setFilteredStocks(filteredStocks);
+    }
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        handleSortByName={handleSortByName}
+        handleSortByPrice={handleSortByPrice}
+        handleFilter={handleFilter}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filteredStocks} buyStock={buyStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer portfolio={portfolio} sellStock={sellStock} />
         </div>
       </div>
     </div>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,25 @@
 import React from "react";
 
-function Stock() {
+function Stock({ stock, buyStock, sellStock, inPortfolio }) {
+  const handleTransaction = () => {
+    if (inPortfolio) {
+      sellStock(stock.id);
+    } else {
+      buyStock(stock);
+    }
+  };
+
   return (
     <div>
-      <div className="card">
+      <div className="card" onClick={handleTransaction}>
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{stock.name}</h5>
+          <p className="card-text">Price: {stock.price}</p>
+          <p className="card-text">Type: {stock.type}</p>
         </div>
       </div>
     </div>
   );
 }
+
 export default Stock;
